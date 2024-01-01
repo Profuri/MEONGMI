8beef04f27975736ef54b176d6aa08196c8649e0
ehdbs28 <doyun5373@naver.com>
2024-01-02 00:56:42 +0900

    player stat 추가

--- Assets/01.Scripts/SO/PlayerStat/PlayerStat.cs
+++ Assets/01.Scripts/SO/PlayerStat/PlayerStat.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
@@ -30,15 +31,48 @@ public class PlayerStat : ScriptableObject
     public Stat gatheringSpeed;
 
     private Dictionary<PlayerStatType, FieldInfo> _fieldInfoDictionary;
-    // private Entity _owner;
+    private Entity _owner;
 
-    // public void SetOwner(Entity owner)
-    // {
-        // _owner = owner;
-    // }
-
-    private IEnumerator IncreaseStatBy(int modifyValue, float duration, PlayerStatType type)
+    public void SetOwner(Entity owner)
     {
-        
+        _owner = owner;
+    }
+
+    private void IncreaseStatBy(int modifyValue, float duration, PlayerStatType type)
+    {
+        _owner.StartCoroutine(StatModifyRoutine(modifyValue, duration, type));
+    }
+
+    private IEnumerator StatModifyRoutine(int modifyValue, float duration, PlayerStatType type)
+    {
+        var stat = GetStatByType(type);
+        stat.AddModifier(modifyValue);
+        yield return new WaitForSeconds(duration);
+        stat.RemoveModifier(modifyValue);
+    }
+
+    private void OnEnable()
+    {
+        _fieldInfoDictionary ??= new Dictionary<PlayerStatType, FieldInfo>();
+        _fieldInfoDictionary.Clear();
+
+        var statType = typeof(PlayerStat);
+        foreach (PlayerStatType type in Enum.GetValues(typeof(PlayerStatType)))
+        {
+            var statField = statType.GetField(type.ToString());
+            if (statField == null)
+            {
+                Debug.LogError($"There are no stat : {type.ToString()}");
+            }
+            else
+            {
+                _fieldInfoDictionary.Add(type, statField);
+            }
+        }
+    }
+
+    public Stat GetStatByType(PlayerStatType type)
+    {
+        return _fieldInfoDictionary[type].GetValue(this) as Stat;
     }
 }
