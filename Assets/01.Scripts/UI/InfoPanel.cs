0b9589cf8b11347d59d75d4c97ac08b991fc1f08
kwak1s1h <kwak1s1h@kakao.com>
2024-01-02 11:16:23 +0900

    FEAT_Tooltip Management System

--- Assets/01.Scripts/UI/InfoPanel.cs
+++ Assets/01.Scripts/UI/InfoPanel.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class InfoPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField] private GameObject _tooltip;
+    [Header("Tooltip")]
+    [SerializeField] private Tooltip _tooltip;
+    [SerializeField] private string _tooltipText = "tooltip";
+    [SerializeField] private float _tooltipWidth = 75f;
+    [SerializeField] private float _tooltipYPos = 10f;
+     
+    [Header("Icon")]
+    [SerializeField] private Sprite _iconSprite;
+    [SerializeField] private Image _icon;
 
     private void Awake()
     {
@@ -14,11 +23,28 @@ public class InfoPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        _tooltip.SetActive(true);
+        _tooltip.SetActive(true, eventData.position);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         _tooltip.SetActive(false);
     }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if(_iconSprite != null && _icon != null)
+        {
+            _icon.sprite = _iconSprite;
+        }
+
+        if (_tooltip != null)
+        {
+            _tooltip.SetText(_tooltipText);
+            _tooltip.SetWidth(_tooltipWidth);
+            _tooltip.SetYPos(_tooltipYPos);
+        }
+    }
+#endif
 }
---
+++ Assets/01.Scripts/UI/Tooltip.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Tooltip : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _text;
+    private RectTransform _rectTransform;
+
+    public void SetText(string text) => _text.SetText(text);
+
+    public void SetWidth(float width)
+    {
+        if(_rectTransform == null)
+        {
+            _rectTransform = transform as RectTransform;
+        }
+
+        Rect rt = _rectTransform.rect;
+        _rectTransform.sizeDelta = new Vector2(width, rt.height);
+    }
+
+    public void SetYPos(float value)
+    {
+        if (_rectTransform == null)
+        {
+            _rectTransform = transform as RectTransform;
+        }
+
+        Vector2 pos = _rectTransform.anchoredPosition;
+        pos.y = value;
+        _rectTransform.anchoredPosition = pos;
+    }
+
+    public void SetActive(bool value, Vector3 position = default)
+    {
+        gameObject.SetActive(value);
+        if(value)
+        {
+            _rectTransform.position = position;
+        }
+    }
+}
