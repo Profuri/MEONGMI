b507a4eb63282e0acbeee194ab2e736bffe5edde
ehdbs28 <doyun5373@naver.com>
2024-01-02 20:21:38 +0900

    풀링 파티클 만듬

--- Assets/01.Scripts/Pool/PoolableParticle.cs
+++ Assets/01.Scripts/Pool/PoolableParticle.cs
@@ -1,7 +1,33 @@
-namespace _01.Scripts.Pool
+using System.Collections;
+using UnityEngine;
+
+public class PoolableParticle : PoolableMono
 {
-    public class PoolableParticle
+    private ParticleSystem _particleSystem;
+    
+    public override void Init()
     {
-        
+        if (_particleSystem is null)
+        {
+            _particleSystem = GetComponent<ParticleSystem>();
+        }
+    }
+
+    public void SetPositionAndRotation(Vector3 pos = default, Quaternion rot = default)
+    {
+        _particleSystem.transform.SetPositionAndRotation(pos, rot);
+    }
+
+    public void Play()
+    {
+        StartCoroutine(PlayRoutine());
+    }
+
+    private IEnumerator PlayRoutine()
+    {
+        _particleSystem.Play();
+        yield return new WaitUntil(() => !_particleSystem.isPlaying);
+        _particleSystem.Stop();
+        PoolManager.Instance.Push(this);
     }
 }
