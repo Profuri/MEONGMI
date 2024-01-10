516d6b7cb80ada9b737989a4ffcf795f3ec8cc61
Nam junseong <danielnam0801@gmail.com>
2024-01-10 13:57:11 +0900

    Feat_CursorAnimationg

--- Assets/01.Scripts/Core/Manager/CursorManager.cs
+++ Assets/01.Scripts/Core/Manager/CursorManager.cs
@@ -2,13 +2,18 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CursorManager : MonoSingleton<CursorManager>
 {
     [SerializeField]
     private Texture2D _baseCursorTexture = null;
+	//private Texture2D curCursorTexture;
 
-	private Texture2D curCursorTexture;
+	[SerializeField]
+	private RectTransform transform_cursor;
+	private Image _cursorImage;
+
 
 	[Header("Base")]
 	[SerializeField] private float baseScale = 1;	
@@ -21,13 +26,14 @@ public class CursorManager : MonoSingleton<CursorManager>
 	[SerializeField] private float TargetScale = 1;
 	[SerializeField] private Color AttackTargetColor = Color.red;
 	[SerializeField] private Color InteractTargetColor = Color.blue;
+	[SerializeField] private Ease animEase;
 
 	private float curScale;
 	private float curAngle;
 	private float baseAngle;
 	private Color curColor;
 
-	public Vector2 pixelsHotSpot;
+	//public Vector2 pixelsHotSpot;
 
 	Vector2 pos;
 	Rect rect;
@@ -42,46 +48,155 @@ public class CursorManager : MonoSingleton<CursorManager>
 
     private void Awake()
     {
-		Cursor.visible = false;
+		Cursor.visible = true;
+		//Cursor.lockState = CursorLockMode.Confined;
 		Init();    
     }
 
     public override void Init()
     {
-		//Cursor.visible = false;
-        SetCursorIcon(_baseCursorTexture);
+		SettingCursor();
+		SetCursorIcon(_baseCursorTexture);
 	}
 
     private void SetCursorIcon(Texture2D texture)
     {
-        Texture2D copyTexture = new Texture2D(
-			(int)((float)texture.width * baseScale),
-			(int)((float)(texture.height * baseScale)));
-        copyTexture.SetPixels(texture.GetPixels());
-        copyTexture.Apply();
+		//      Texture2D copyTexture = new Texture2D(
+		//	(int)((float)texture.width * baseScale),
+		//	(int)((float)(texture.height * baseScale)));
+		//      copyTexture.SetPixels(texture.GetPixels());
+		//      copyTexture.Apply();
 
-        curCursorTexture = copyTexture;
+		//      curCursorTexture = copyTexture;
 
-		pixelsHotSpot = new Vector2(
-			curCursorTexture.width * curScale / 2
-			, curCursorTexture.height * curScale / 2
-		);
+		//pixelsHotSpot = new Vector2(
+		//	curCursorTexture.width * curScale / 2
+		//	, curCursorTexture.height * curScale / 2
+		//);
 		// pixelsHotSpot = new Vector2(curCursorTexture.width / 2f, curCursorTexture.height / 2f);
 		//Cursor.SetCursor(_baseCursorTexture,
 		//    new Vector2(_baseCursorTexture.width / 2f, _baseCursorTexture.height / 2f),
 		//    CursorMode.Auto);
 
-		//Cursor.SetCursor(curCursorTexture,
-		//    new Vector2(pixelsHotSpot.x, pixelsHotSpot.y),
-		//    CursorMode.ForceSoftware);
+		//     Cursor.SetCursor(curCursorTexture,
+		//pixelsHotSpot,
+		//         CursorMode.ForceSoftware);
 
-		Cursor.SetCursor(curCursorTexture,
-			pixelsHotSpot,
-            CursorMode.Auto);
+		//     Cursor.SetCursor(curCursorTexture,
+		//pixelsHotSpot,
+		//         CursorMode.Auto);
 
-        SetTextureColor(baseColor);
-        curScale = baseScale;
+		//SetScaleTexture(2f);
+		//SetTextureColor(baseColor);
+	}
+
+	private void SettingCursor()
+    {
+		Cursor.visible = false;
+		//transform_cursor.pivot = Vector2.up;
+
+		if (transform_cursor.GetComponent<Graphic>())
+			transform_cursor.GetComponent<Graphic>().raycastTarget = false;
+
+		_cursorImage = transform_cursor.GetComponent<Image>();
+		curScale = baseScale;
 		curColor = baseColor;
+	}
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnAttack();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            OnInteract();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            OnBase();
+        }
+
+        UpdateCursorPosition();
+    }
+
+    private void UpdateCursorPosition()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        transform_cursor.position = mousePos;
+    }
+
+    public void OnBase()
+	{
+		_cursorImage.DOKill();
+		transform_cursor.DOKill();
+		_cursorImage.DOColor(baseColor, animTime).SetEase(animEase); ;
+		transform_cursor.DOScale(baseScale, animTime).SetEase(animEase); ;
+		//Vector3 rotateValue = new Vector3(0, 0, 0);
+		//Quaternion quaternion = Quaternion.Euler(0, 0, 0);
+		//transform_cursor.DORotateQuaternion(quaternion, animTime).SetEase(animEase);
+		//DOTween.To(() => curColor, color =>
+		//{
+		//	this.curColor = color;
+		//	SetTextureColor(this.curColor);
+		//}, baseColor, animTime);
+		DOTween.To(() => curAngle, angle =>
+		{
+			this.curAngle = angle;
+			transform_cursor.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+		}, baseAngle, animTime).SetEase(animEase);
+	}
+
+	public void OnInteract()
+    {
+		_cursorImage.DOKill();
+		_cursorImage.rectTransform.DOKill();
+		_cursorImage.DOColor(InteractTargetColor, animTime).SetEase(animEase); ;
+		_cursorImage.rectTransform.DOScale(baseScale, animTime).SetEase(animEase); ;
+		//Vector3 rotateValue = new Vector3(0, 0, 0);
+		//Quaternion quaternion = Quaternion.Euler(0, 0, 0);
+		//transform_cursor.DORotateQuaternion(quaternion, animTime).SetEase(animEase);
+		//DOTween.To(() => curColor, color =>
+		//{
+		//	this.curColor = color;
+		//	SetTextureColor(this.curColor);
+		//}, InteractTargetColor, animTime);
+		DOTween.To(() => curAngle, angle =>
+		{
+			this.curAngle = angle;
+			transform_cursor.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+		}, baseAngle, animTime).SetEase(animEase);
+	}
+
+	public void OnAttack()
+    {
+		_cursorImage.DOKill();
+		_cursorImage.rectTransform.DOKill();
+		_cursorImage.DOColor(AttackTargetColor,animTime).SetEase(animEase); ;
+		_cursorImage.rectTransform.DOScale(TargetScale, animTime).SetEase(animEase); ;
+		//Vector3 rotateValue = new Vector3(0, 0, TargetAngle);
+		//Quaternion quaternion = Quaternion.Euler(0, 0, TargetAngle);
+		//transform_cursor.DORotate(quaternion, animTime).SetEase(animEase);
+        //DOTween.To(() => curColor, color =>
+        //{
+        //	this.curColor = color;
+        //	SetTextureColor(this.curColor);
+        //}, AttackTargetColor, animTime);
+
+        DOTween.To(() => curAngle, angle =>
+        {
+            this.curAngle = angle;
+			transform_cursor.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+		}, TargetAngle, animTime).SetEase(animEase);
+    }
+
+	public void Shoot()
+	{
+
 	}
 
 	private void SetTextureColor(Color color)
@@ -106,101 +221,68 @@ public class CursorManager : MonoSingleton<CursorManager>
             fillColorArray[i] *= fillColor;
         }
 
-		//      for (int y = 0; y < cursorTexture.height; y++)
-		//{
-		//	for (int x = 0; x < cursorTexture.width; x++)
-		//	{
-		//		cursorTexture.SetPixel(x, y, color);
-		//	}
-		//}
-		curCursorTexture.SetPixels(fillColorArray);
-		curCursorTexture.Apply();
+		//curCursorTexture.SetPixels(fillColorArray);
+		//curCursorTexture.Apply();
 	}
-
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-			OnAttack();
-		}
-
-		if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-			OnInteract();
-		}
-
-		if(Input.GetKeyDown(KeyCode.RightShift))
-        {
-			OnBase();
-		}
-
-	}
-
-	public void OnBase()
-	{
-		DOTween.To(() => curColor, color =>
-		{
-			this.curColor = color;
-			SetTextureColor(this.curColor);
-		}, baseColor, animTime);
-	}
-
-	public void OnInteract()
-    {
-		DOTween.To(() => curColor, color =>
-		{
-			this.curColor = color;
-			SetTextureColor(this.curColor);
-		}, InteractTargetColor, animTime);
-	}
-
-	public void OnAttack()
-    {
-		DOTween.To(() => curColor, color =>
-		{
-			this.curColor = color;
-			SetTextureColor(this.curColor);
-		}, AttackTargetColor, animTime);
-	}
-
-	public void Shoot()
-    {
-
-    }
-
     void OnGUI()
 	{
-        updateTexture();
+        //updateTexture();
 
-        Matrix4x4 back = GUI.matrix;
-        GUIUtility.RotateAroundPivot(curAngle, pos);
-        GUI.DrawTexture(rect, curCursorTexture);
-        GUI.matrix = back;
+        //Matrix4x4 back = GUI.matrix;
+        //GUIUtility.RotateAroundPivot(curAngle, pos);
+        //GUI.DrawTexture(rect, curCursorTexture);
+        //GUI.matrix = back;
     }
 
 	void updateTexture()
 	{
-		pos = Input.mousePosition;
-		pos.y *= -1f;
-		//pos.y = Screen.height - pos.y;
+		//pos = Input.mousePosition;
+		//pos.y *= -1f;
+		////pos.y = Screen.height - pos.y;
 
-		//float last_angle = angle;
-		//	float new_angle = Mathf.Atan2(pos.y - prev_position.y, pos.x - prev_position.x) * Mathf.Rad2Deg;
-		//	angle = Mathf.MoveTowardsAngle(
-		//		last_angle,
-		//		Mathf.LerpAngle(last_angle, new_angle, 1f - smoothness),
-		//		10f
-		//		);
+		////float last_angle = angle;
+		////	float new_angle = Mathf.Atan2(pos.y - prev_position.y, pos.x - prev_position.x) * Mathf.Rad2Deg;
+		////	angle = Mathf.MoveTowardsAngle(
+		////		last_angle,
+		////		Mathf.LerpAngle(last_angle, new_angle, 1f - smoothness),
+		////		10f
+		////		);
 		
 
-		//prev_position = pos;
+		////prev_position = pos;
 
-		pixelsHotSpot = new Vector2(
-			curCursorTexture.width * curScale / 2
-			,curCursorTexture.height * curScale / 2
-		);
+		//pixelsHotSpot = new Vector2(
+		//	curCursorTexture.width * curScale / 2
+		//	,curCursorTexture.height * curScale / 2
+		//);
 
-		rect = 
-			new Rect(pos.x - pixelsHotSpot.x, pos.y - pixelsHotSpot.y, curCursorTexture.width * curScale, curCursorTexture.height * curScale);
+
+		//rect = 
+		//	new Rect(pos.x - pixelsHotSpot.x, pos.y - pixelsHotSpot.y, curCursorTexture.width * curScale, curCursorTexture.height * curScale);
+	}
+
+	public void SetScaleTexture(float _scaleFactor)
+	{
+		//int _newWidth = Mathf.RoundToInt(curCursorTexture.width * _scaleFactor);
+		//int _newHeight = Mathf.RoundToInt(curCursorTexture.height * _scaleFactor);
+
+		//Color[] _scaledTexPixels = new Color[_newWidth * _newHeight];
+
+		//for (int _yCord = 0; _yCord < _newHeight; _yCord++)
+		//{
+		//	float _vCord = _yCord / (_newHeight * 1f);
+		//	int _scanLineIndex = _yCord * _newWidth;
+
+		//	for (int _xCord = 0; _xCord < _newWidth; _xCord++)
+		//	{
+		//		float _uCord = _xCord / (_newWidth * 1f);
+
+		//		_scaledTexPixels[_scanLineIndex + _xCord] = curCursorTexture.GetPixelBilinear(_uCord, _vCord);
+		//	}
+		//}
+
+		//// Create Scaled Texture
+		//curCursorTexture.SetPixels(_scaledTexPixels, 0);
+		//curCursorTexture.Apply();
 	}
 }
